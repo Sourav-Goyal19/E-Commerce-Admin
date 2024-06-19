d3bf5a99aa4647353b0f6e6ec21e8a5a957a55cc
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-06-19 10:12:15 +0530

    redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,7 +2,7 @@ import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
   url: "https://unified-moose-49271.upstash.io",
-  token: "AcB3AAIncDE5MGNlY2ViOGIzZjg0M2MxODM1NTMzYzQ0YjM4MjdhMHAxNDkyNzE",
+  token: process.env.NEXT_PUBLIC_REDIS_KEY,
 });
 
 export default redis;
